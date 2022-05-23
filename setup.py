0577c59b88e4961c4517b72fad969141cbe7e73e
Ammon21 <ammonb6@gmail.com>
2022-05-23 17:58:13 +0300

    setting up repository

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['pandas>=1.1.0', 'numpy>=1.19.0', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Ammon Leulseged",
+    email="Ammonb6@gmail.com",
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Pharmaceutical Sales Prediction",
+    install_requires=requirements,
+    long_description=readme,
+    include_package_data=True,
+    keywords='scripts',
+    name='scripts',
+    packages=find_packages(include=['scripts', 'scripts.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Ammon21/Rossmann_Pharmaceutical_Sales',
+    version='0.1.0',
+    zip_safe=False,
+)
